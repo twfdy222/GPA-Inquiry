04f75752713e945c7ec29311c391b5cbc52e6c16
twfdy222 <278593138+twfdy222@users.noreply.github.com>
2026-05-11 22:03:46 +0800

    Polish V2.1 metadata and docs

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gpa-inquiry-v2-shell'
+const CACHE_NAME = 'gpa-inquiry-v2.1-shell'
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest']
 
 self.addEventListener('install', (event) => {
